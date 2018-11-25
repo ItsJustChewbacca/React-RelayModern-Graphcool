ecd98da54887ebdb3675ea81b757c30cbf42ebb4
Luke Baziw-Grossmann <luke@Lukes-MacBook-Pro.local>
2018-11-25 15:24:52 -0800

    added queryRenderer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import ListPage from './ListPage'
+import {
+  QueryRenderer,
+  graphql
+} from 'react-relay'
+import environment from './Environment'
 class App extends Component {
   render() {
     return (
