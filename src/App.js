e00995c990298cbc64309dabe8938478b6ade35d
Luke Baziw-Grossmann <luke@Lukes-MacBook-Pro.local>
2018-11-25 15:25:41 -0800

    added root query

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import {
   graphql
 } from 'react-relay'
 import environment from './Environment'
+const AppAllPostQuery = graphql`
+  query AppAllPostQuery {
+    viewer {
+      ...ListPage_viewer
+    }
+  }
+`
 class App extends Component {
   render() {
     return (
