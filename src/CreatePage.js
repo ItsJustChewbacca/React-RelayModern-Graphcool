c5d7f3c477b228f88143846aa293ff344f94ed4b
Luke Baziw-Grossmann <luke@Lukes-MacBook-Pro.local>
2018-11-25 17:17:07 -0800

    Added query renderer, enviroment, updated handlePost method

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,4 +1,15 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+import CreatePostMutation from './CreatePostMutation'
+import { QueryRenderer, graphql } from 'react-relay'
+import environment from './Environments'
+const CreatePageViewerQuery = graphql`
+  query CreatePageViewerQuery {
+    viewer {
+      id
+    }
+  }
+`
 class CreatePage extends React.Component {
     state = {
         description: '',
@@ -6,33 +17,49 @@ class CreatePage extends React.Component {
     }
 
     render () {
-        return (
-        <div className='w-100 pa4 flex justify-center'>
-            <div style={{ maxWidth: 400 }} className=''>
-            <input
-                className='w-100 pa3 mv2'
-                value={this.state.description}
-                placeholder='Description'
-                onChange={(e) => this.setState({description: e.target.value})}
-            />
-            <input
-                className='w-100 pa3 mv2'
-                value={this.state.imageUrl}
-                placeholder='Image Url'
-                onChange={(e) => this.setState({imageUrl: e.target.value})}
-            />
-            {this.state.imageUrl &&
-            <img src={this.state.imageUrl} className='w-100 mv3' />
+      return (
+        <QueryRenderer
+          environment={environment}
+          query={CreatePageViewerQuery}
+          render={({error, props}) => {
+            if (error) {
+              return <div>{error.message}</div>
+            } else if (props) {
+              return (
+                // --- previous implementation ----
+                <div className='w-100 pa4 flex justify-center'>
+                  <div style={{ maxWidth: 400 }} className=''>
+                    <input
+                      className='w-100 pa3 mv2'
+                      value={this.state.description}
+                      placeholder='Description'
+                      onChange={(e) => this.setState({description: e.target.value})}
+                    />
+                    <input
+                      className='w-100 pa3 mv2'
+                      value={this.state.imageUrl}
+                      placeholder='Image Url'
+                      onChange={(e) => this.setState({imageUrl: e.target.value})}
+                    />
+                    {this.state.imageUrl &&
+                    <img src={this.state.imageUrl} className='w-100 mv3' />
+                    }
+                    {this.state.description && this.state.imageUrl &&
+                    <button className='pa3 bg-black-10 bn dim ttu pointer' onClick={() => this._handlePost(props.viewer.id)}>Post</button>
+                    }
+                  </div>
+                </div>
+                // --- previous implementation ----
+              )
             }
-            {this.state.description && this.state.imageUrl &&
-            <button className='pa3 bg-black-10 bn dim ttu pointer' onClick={this._handlePost}>Post</button>
-            }
-            </div>
-        </div>
-        )
+            return <div>Loading</div>
+          }}
+        />
+      )
     }
-    _handlePost = () => {
-        // ... you'll implement this in a bit
+    _handlePost = (viewerId) => {
+      const {description, imageUrl} = this.state
+      CreatePostMutation(description, imageUrl, viewerId, () => this.props.history.replace('/'))
     }
 }
-export default CreatePage
+export default (CreatePage)
