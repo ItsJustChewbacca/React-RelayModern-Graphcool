ab96333f4f1de4d66de8a8c0be5b8a2bb1161bd1
Luke Baziw-Grossmann <luke@Lukes-MacBook-Pro.local>
2018-11-25 16:13:41 -0800

    updated react compatibility with relay modern doc

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Post from './Post'
+import { Link } from 'react-router-dom';
 import {
   createFragmentContainer,
   graphql
@@ -24,6 +25,9 @@ class ListPage extends React.Component {
   render () {
     return (
       <div className='w-100 flex justify-center'>
+        <Link to='/create' className='fixed bg-white top-0 right-0 pa4 ttu dim black no-underline'>
+          + New Post
+        </Link>
         <div className='w-100' style={{ maxWidth: 400 }}>
           {this.props.viewer.allPosts.edges.map(({node}) =>
             <Post key={node.__id} post={node} />
